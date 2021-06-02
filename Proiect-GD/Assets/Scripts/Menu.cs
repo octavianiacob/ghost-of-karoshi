122db3f76db9437325d90f6953a36791d4edbbb1
andreiprepelita <andrei_prepelita@yahoo.com>
2021-06-02 13:16:16 +0300

    executabil final + componente joc

--- Proiect-GD/Assets/Scripts/Menu.cs
+++ Proiect-GD/Assets/Scripts/Menu.cs
@@ -5,8 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    void Start()
+    {
+        GameObject gameObject = GameObject.Find("HowToPlayMenu");
+        if (gameObject)
+        gameObject.SetActive(false);
+        /*gameObject.SetActive(false);*/
+    }
     public void StartFirstLevel()
     {
         SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+       
+    }
+    public void GoToHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlayMenu");
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
---
+++ Proiect-GD/Assets/Scripts/Redirect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Redirect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+    IEnumerator waiter()
+    {
+       
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
